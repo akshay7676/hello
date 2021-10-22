561cac4c5e1851d43c5afb58f4db980b91ad0b6a
USER <USER@LAPTOP-F5CLJVFB>
2021-10-22 20:22:47 +0530

    first commit.....

---
+++ hello/src/test/java/hello/testhello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.testng.annotations.Test;
+
+public class testhello {
+	
+	public static void main(String[] args) {
+		
+		
+
+			System.out.println("...hello aksh");
+		
+	}
+}
